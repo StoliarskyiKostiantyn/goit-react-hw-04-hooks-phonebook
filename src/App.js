40890f4f48e275e91f57e58e71629d21106c246d
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-09-21 00:42:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import "./App.css";
 import Phonebook from "./Components/Phonebook";
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import ContactList from "./Components/ContactList";
 import Filter from "./Components/Filter";
 import shortid from "shortid";
 export default function App() {
-  const [contacts, setContacts] = useState(
+  const [contacts, setContacts] = useState([
     { id: "id-1", name: "Rosie Simpson", phone: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", phone: "443-89-12" },
     { id: "id-3", name: "Eden Clements", phone: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", phone: "227-91-26" }
-  );
-  const [filter, setFilter] = useState("initialState");
+    { id: "id-4", name: "Annie Copeland", phone: "227-91-26" },
+  ]);
+  const [filterValue, setFilterValue] = useState("");
   const forSubmiHandler = (data) => {
     const item = {
       id: shortid.generate(),
       ...data,
     };
-    setContacts((prevState) => ({
-      contacts: [...prevState.contacts, item],
-    }));
+    setContacts((prevState) => [...prevState, item]);
   };
   const onFilterChange = (e) => {
-    setFilter(e.currentTarget.value);
+    setFilterValue(e.currentTarget.value);
   };
   const getVisibleContact = () => {
-    const normalizaedFilter = filter.toLowerCase();
+    const normalizaedFilter = filterValue.toLowerCase();
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(normalizaedFilter)
     );
@@ -40,7 +38,7 @@ export default function App() {
     <>
       <h1>Phonebook</h1>
       <Phonebook onSubmit={forSubmiHandler}></Phonebook>
-      <Filter value={filter} onChange={onFilterChange} />
+      <Filter value={filterValue} onChange={onFilterChange} />
       <h2>Contacts</h2>
       <ContactList
         contactitems={visibleContact}
@@ -49,36 +47,3 @@ export default function App() {
     </>
   );
 }
-
-// class oldApp extends Component {
-//   state = {
-//     contacts: [
-//       { id: "id-1", name: "Rosie Simpson", phone: "459-12-56" },
-//       { id: "id-2", name: "Hermione Kline", phone: "443-89-12" },
-//       { id: "id-3", name: "Eden Clements", phone: "645-17-79" },
-//       { id: "id-4", name: "Annie Copeland", phone: "227-91-26" },
-//     ],
-//     filter: "",
-//   };
-//   forSubmiHandler = (data) => {
-//     const item = {
-//       id: shortid.generate(),
-//       ...data,
-//     };
-//     this.setState((prevState) => ({
-//       contacts: [...prevState.contacts, item],
-//     }));
-//   };
-//   onFilterChange = (e) => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-//   getVisibleContact = () => {
-//     const { filter, contacts } = this.state;
-//     const normalizaedFilter = filter.toLowerCase();
-//     return contacts.filter((contact) =>
-//       contact.name.toLowerCase().includes(normalizaedFilter)
-//     );
-//   };
-//   onDeleteContact = (id) => {
-//     this.setState((prevState) => ({
-//
